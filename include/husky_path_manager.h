b42d4b81755d94344620465394dd5e0dd869c54c
Dallin Briggs <dallinbriggs@gmail.com>
2016-05-26 18:13:19 -0600

    Trying to get it to publish

--- include/husky_path_manager.h
+++ include/husky_path_manager.h
@@ -2,6 +2,7 @@
 #define HUSKY_PATH_MANAGER_H
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
+#include <geometry_msgs/Twist.h>
 
 namespace husky_path_manager {
 
@@ -10,6 +11,12 @@ class huskyPathManager
 {
 public:
   huskyPathManager();
+
+private:
+  ros::NodeHandle nh;
+  ros::Publisher cmd_vel_pub;
+  geometry_msgs::Twist base_cmd;
+
 };
 
 } //end namespace
--- src/husky_path_manager.cpp
+++ src/husky_path_manager.cpp
@@ -7,6 +7,21 @@ namespace husky_path_manager {
 
 huskyPathManager::huskyPathManager()
 {
+
+//  tf::TransformListener tf_listener;
+  cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel",1);
+
+  base_cmd.linear.x = 0.7;
+  base_cmd.linear.y = 0;
+  base_cmd.linear.z = 0;
+  base_cmd.angular.x = 0;
+  base_cmd.angular.y = 0;
+  base_cmd.angular.z = 0.15;
+
+  cmd_vel_pub.publish(base_cmd);
+
+
+
 }
 
 } //end namespace
@@ -15,8 +30,7 @@ int main (int argc, char** argv)
 {
   ros::init(argc, argv, "husky_path_manager");
   ros::NodeHandle nh;
-  husky_path_manager::huskyPathManager object;
-
+  husky_path_manager::huskyPathManager* cont = new husky_path_manager::huskyPathManager;
 
   ros::spin();
 
