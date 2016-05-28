a3015f42c95399803db54c8bc741b964a9379120
Dallin Briggs <dallinbriggs@gmail.com>
2016-05-27 20:42:51 -0600

    Added a publisher test node

--- include/husky_path_manager.h
+++ include/husky_path_manager.h
@@ -3,6 +3,8 @@
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
 #include <geometry_msgs/Twist.h>
+#include <std_msgs/String.h>
+#include <sstream>
 
 namespace husky_path_manager {
 
@@ -16,6 +18,12 @@ private:
   ros::NodeHandle nh;
   ros::Publisher cmd_vel_pub;
   geometry_msgs::Twist base_cmd;
+
+  ros::NodeHandle n;
+  ros::Publisher chatter_pub;
+//  ros::Rate loop_rate;
+  int count;
+  std::stringstream ss;
 
 };
 
--- src/husky_path_manager.cpp
+++ src/husky_path_manager.cpp
@@ -9,16 +9,18 @@ huskyPathManager::huskyPathManager()
 {
 
 //  tf::TransformListener tf_listener;
-  cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel",1);
+//  cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel",1);
 
-  base_cmd.linear.x = 0.7;
-  base_cmd.linear.y = 0;
-  base_cmd.linear.z = 0;
-  base_cmd.angular.x = 0;
-  base_cmd.angular.y = 0;
-  base_cmd.angular.z = 0.15;
+//  base_cmd.linear.x = 0.7;
+//  base_cmd.linear.y = 0;
+//  base_cmd.linear.z = 0;
+//  base_cmd.angular.x = 0;
+//  base_cmd.angular.y = 0;
+//  base_cmd.angular.z = 0.15;
 
-  cmd_vel_pub.publish(base_cmd);
+//  cmd_vel_pub.publish(base_cmd);
+
+
 
 
 
@@ -30,7 +32,8 @@ int main (int argc, char** argv)
 {
   ros::init(argc, argv, "husky_path_manager");
   ros::NodeHandle nh;
-  husky_path_manager::huskyPathManager* cont = new husky_path_manager::huskyPathManager;
+//  husky_path_manager::huskyPathManager* cont = new husky_path_manager::huskyPathManager;
+  husky_path_manager::huskyPathManager thing;
 
   ros::spin();
 
